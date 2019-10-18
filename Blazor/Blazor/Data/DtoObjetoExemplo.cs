e8d520da895eb2b4a512b5155ab5b373de2f8c72
paulorgn <42241426+paulorgn@users.noreply.github.com>
2019-10-18 18:41:24 -0300

    Blz

---
+++ Blazor/Blazor/Data/DtoObjetoExemplo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.Data
+{
+    public class DtoObjetoExemplo
+    {
+        public string title { get; set; }
+        public string body { get; set; }
+        public string userId { get; set; }
+        public int id { get; set; }
+    }
+}
---
+++ Blazor/Blazor/Data/Livros.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Blazor.Data
+{
+    public class Livros
+    {
+        public HttpClient IniciarCliente()
+        {
+            try
+            {
+                HttpClient cliente = new HttpClient();
+
+                cliente.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+                cliente.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                return cliente;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public HttpResponseMessage ClienteChamarAAPI()
+        {
+            try
+            {
+                HttpClient cliente = IniciarCliente();
+                HttpResponseMessage resposta = cliente.GetAsync("/posts").Result;
+
+                return resposta;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<DtoObjetoExemplo> PegarRetornoDaAPI()
+        {
+            try
+            {
+                HttpResponseMessage resposta = ClienteChamarAAPI();
+
+                if (resposta.IsSuccessStatusCode)
+                {
+                    string stringRetornoDaApi = resposta.Content.ReadAsStringAsync().Result.ToString();
+                    List<DtoObjetoExemplo> retornoDaAPI =  JsonConvert.DeserializeObject<List<DtoObjetoExemplo>>(stringRetornoDaApi);
+
+                    return retornoDaAPI;
+                }
+                else
+                {
+                    throw new Exception("A requisição não obteve uma resposta do servidor");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
